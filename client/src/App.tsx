fdcddb4bab85e132c3d7cc18d6f465e73e6cb5e3
Fran-Cilia <francisco.cilia@gmail.com>
2024-04-21 14:42:44 -0700

    basic PDF Menu components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,52 +1,22 @@
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import ky from "ky";
-import { useForm, SubmitHandler } from "react-hook-form";
+import React from "react";
+import PDFPreview from "./components/PDFPreview";
+import "./App.css";
 
 function App() {
-  const queryClient = useQueryClient();
+  const PDF_TITLES = ["Sommerville", "PDF Title 2", "PDF Title 3", "PDF Title 4"];
 
-  const { data, isLoading, error } = useQuery({
-    queryKey: ["users"],
-    queryFn: async () => {
-      const response = await ky.get("http://localhost:3000/getUsers").json();
-
-      console.log(`RESPONSE: ${JSON.stringify(response)}`);
-
-      return response;
-    },
-  });
-
-  const mutation = useMutation({
-    mutationFn: ({ name }: { name: string }) => {
-      console.log(`CREATING: ${name}`);
-      return ky.post("http://localhost:3000/createUser", { json: { name } });
-    },
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["users"] });
-    },
-  });
-
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm<{ name: string }>();
+  const handlePDFClick = (title: string) => {
+    // TODO: Handle PDF redirection/routing here
+    console.log("PDF clicked:", title);
+  };
 
   return (
-    <div className="flex flex-col items-center">
-      <h1>Users: {JSON.stringify(data)}</h1>
-      <div>
-        <h1>Create New User:</h1>
-        <form
-          onSubmit={handleSubmit((data) => {
-            mutation.mutate(data);
-          })}
-        >
-          <h1>Name:</h1>
-          <input {...register("name")} className="border-black border-2" />
-        </form>
-      </div>
+    <div className="pdf-menu">
+      {/* TODO: Add navbar */}
+      <h1>Menu</h1>
+      {PDF_TITLES.map((title, index) => (
+        <PDFPreview key={index} title={title} onClick={() => handlePDFClick(title)} />
+      ))}
     </div>
   );
 }
