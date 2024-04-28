487daa16333f1769a049c66bd10290c113ddc0b0
Abhinav Palacharla <abhinav.palacharla@gmail.com>
2024-04-28 15:00:11 -0700

    🛠 sq3R->ARA

--- client/src/views/Login.tsx
+++ client/src/views/Login.tsx
@@ -41,7 +41,7 @@ const Login = () => {
   return (
     data && (
       <div className="h-screen flex flex-col items-center">
-        <h1 className="mt-24 text-4xl font-semibold">Welcome to SQ3R</h1>
+        <h1 className="mt-24 text-4xl font-semibold">Welcome to ARA</h1>
         <h1 className="mt-12 text-4xl font-semibold">Select User</h1>
 
         <div className="flex flex-row items-center gap-x-8 mt-24">
