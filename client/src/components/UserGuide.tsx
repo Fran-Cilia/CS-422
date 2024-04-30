d26407ea1489e40b01fcb54d8d20b8e9aa00872d
ValerADHD <valeradhd@gmail.com>
2024-04-29 18:32:19 -0700

    programmer documentation

--- client/src/components/UserGuide.tsx
+++ client/src/components/UserGuide.tsx
@@ -1,8 +1,12 @@
+/// AUTHORS: NA, FC, VD, RK, AP
+/// LAST EDITED: 4-29-2024
+/// DESCRIPTION: UserGuide.tsx: Describes the component that controls the step-by-step intro guide to SQ3R.
 import React, { useState } from "react";
 
 const UserGuide = () => {
   const [currentStep, setCurrentStep] = useState<number>(0);
 
+  //array of text displays for each step in the SQ3R process
   const steps = [
     "SURVEY: Skim the text focusing on the main ideas (chapters, headers, diagrams, models etc.",
     "QUESTION: Formulate questions based on the observed main ideas.",
@@ -11,6 +15,7 @@ const UserGuide = () => {
     "REVIEW: Review the material by reapeating back to yourself the purpose of the passage.",
   ];
 
+  //return component data
   return (
     <div className="border-[1px] border-[#e4e4e7] rounded-lg my-8 p-4">
       <h1 className="border-[1.5px] border-[#e4e4e7] rounded-lg p-2 w-8 h-8 flex flex-row items-center justify-center font-medium text-black text-lg">
@@ -23,6 +28,7 @@ const UserGuide = () => {
           <button
             className="border-[1px] border-[#e4e4e7] rounded-md px-2 py-1"
             onClick={() => {
+              // if previous button is clicked, reduce current step by one (if non-zero)
               if (currentStep > 0) {
                 setCurrentStep(currentStep - 1);
               }
@@ -33,6 +39,7 @@ const UserGuide = () => {
           <button
             className="border-[1px] border-[#e4e4e7] rounded-md px-2 py-1"
             onClick={() => {
+              //if next button is clicked, increase current step by one (if not last step)
               if (currentStep < steps.length - 1) {
                 setCurrentStep(currentStep + 1);
               }
--- server/src/scripts/initDB.ts
+++ server/src/scripts/initDB.ts
@@ -81,6 +81,78 @@ const PDFs = [
             "Id aute non voluptate. Cillum quis nulla labore pariatur. Qui ex consequat enim proident ipsum veniam officia sit est. Irure ad dolore nostrud sint veniam qui aute Lorem occaecat ad.",
           pdfId: id,
         });
+        await db.insert(notes).values({
+          chapter: "Chapter 22",
+          header: "22.2 Managing People",
+          question:
+            "Why should a project manager prioritize people over everything?",
+          answer: 
+            "It is more expensive in both money and productivity to recruit new engineers. A good project manager will focus on keeping engineers with the team and being productive in order to effectively complete tasks. On top of this, making sure people are working on tasks that they are effective at ensures high productivity.",
+          pdfId: id,
+        });
+        await db.insert(notes).values({
+          chapter: "Chapter 22",
+          header: "22.2 Managing People",
+          question:
+            "What factors influence how a manager works with their subordinates?",
+          answer: 
+            "1. Consistency- a good project manager should treat their employees in a predictable manner (treating everyone the same, ensure that nothing is unexpected) 2. Respect- a good project manager should respect the skills and differences in all of their employees. 3. Inclusion- everyone should feel able to contribute to the project 4. Honesty",
+          pdfId: id,
+        });
+        await db.insert(notes).values({
+          chapter: "Chapter 22",
+          header: "22.2.1 Motivating People",
+          question:
+            "Why should a project manager motivate people?",
+          answer: 
+            "An unmotivated engineer gets much less work done than a well motivated one. On top of this, unmotivated engineers will make more mistakes than normal. In order to have a productive team, everyone must be motivated to do their work.",
+          pdfId: id,
+        });
+        await db.insert(notes).values({
+          chapter: "Chapter 22",
+          header: "22.2.1 Motivating People",
+          question:
+            "How should a project manager motivate their employees?",
+          answer: 
+            "A manager should focus on an employees needs in order to determine how they should be motivated. Social needs should be met by giving them opportunities to meet with coworkers and other team members. Esteem needs should be met by showing people they are valued by their organization. \"self realization\" needs should be met by giving people responsibilities and providing opportunities for development.",
+          pdfId: id,
+        });
+        await db.insert(notes).values({
+          chapter: "Chapter 22",
+          header: "22.2.1 Motivating People",
+          question:
+            "Why should a project manager motivate people?",
+          answer: 
+            "An unmotivated engineer gets much less work done than a well motivated one. On top of this, unmotivated engineers will make more mistakes than normal. In order to have a productive team, everyone must be motivated to do their work.",
+          pdfId: id,
+        });
+        await db.insert(notes).values({
+          chapter: "Chapter 22",
+          header: "22.2.1 Motivating People",
+          question:
+            "How should a project manager motivate their employees?",
+          answer: 
+            "A manager should focus on an employees needs in order to determine how they should be motivated. Social needs should be met by giving them opportunities to meet with coworkers and other team members. Esteem needs should be met by showing people they are valued by their organization. \"self realization\" needs should be met by giving people responsibilities and providing opportunities for development.",
+          pdfId: id,
+        });
+        await db.insert(notes).values({
+          chapter: "Chapter 22",
+          header: "22.2.1 Motivating People",
+          question:
+            "What motivates people to work in an efficient manner?",
+          answer: 
+            "People tend to work in an efficient manner when their needs are met. A project manager should consider the needs of each person on their team in order to help them achieve satisfaction and therefore motivation.",
+          pdfId: id,
+        });
+        await db.insert(notes).values({
+          chapter: "Chapter 22",
+          header: "22.2.1 Motivating People",
+          question:
+            "What needs might a software engineer have in the corporate environment?",
+          answer: 
+            "Software engineers typically have most of their basic needs met. In a corporate environment, it is important to focus on social needs (interaction), esteem needs(being valued), and self-realization needs (feeling skilled).",
+          pdfId: id,
+        });
       })
     );
   });
